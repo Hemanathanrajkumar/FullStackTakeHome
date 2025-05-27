8339982f7be0e9a0adda60401862f4878f03ed6e
Hemanathan <hemanathanrajkumar@gmail.com>
2025-05-27 14:02:16 +0530

    App routing updated

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # FullStackTakeHome
+## Please install the following versions for Each 
+## Frontend - Angular: 16.2.12
+## Backend - Node v24.0.2, NPM 11.3.0
+
+
+First clone the project using Git Clone 
+Once clonned please execute the following cmds
+
+cd front-end-app
+npm install 
+cd ..
+cd backend 
+npm install
+
+
+To run the Angular Application
+cd front-end-app
+ng serve
+
+To run the Backedn Application
+cd backend 
+npm start 
--- front-end-app/src/app/app-routing.module.ts
+++ front-end-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  }, 
+  {
+    path: 'home',
+    loadChildren: () => import('../app/landing-page/landing-page.module').then((m) => m.LandingPageModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
