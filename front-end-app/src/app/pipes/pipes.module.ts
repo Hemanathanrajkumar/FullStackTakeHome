eededd5d11e91508a639d14e265cada4b5706981
Hemanathan <hemanathanrajkumar@gmail.com>
2025-05-27 14:02:51 +0530

    pipes created

---
+++ front-end-app/src/app/pipes/pipes.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SortPipe } from './sort.pipe';
+import { FilterPipe } from './filter.pipe';
+
+
+
+@NgModule({
+  declarations: [
+    SortPipe,
+    FilterPipe
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PipesModule { }
